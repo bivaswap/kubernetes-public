397f9d8a8c5c6453e64fbfdc31011f7d5c2c702e
Bivas Mandal <bivaswap@users.noreply.github.com>
2019-08-20 21:23:26 +0530

    Update bootstrap-kube.sh

--- bootstrap-kube.sh
+++ bootstrap-kube.sh
@@ -1,11 +1,11 @@
-apt-get update && apt-get install apt-transport-https ca-certificates curl software-properties-common
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
   stable"
 
-apt-get update && apt-get install docker-ce=5:18.09.8~3-0~ubuntu-bionic docker-ce-cli=5:18.09.8~3-0~ubuntu-bionic
+apt-get update && apt-get install -y docker-ce=5:18.09.8~3-0~ubuntu-bionic docker-ce-cli=5:18.09.8~3-0~ubuntu-bionic
 cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
